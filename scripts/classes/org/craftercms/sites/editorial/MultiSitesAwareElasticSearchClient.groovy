de810bba17b4dc8bacb0a1be07a49ecfbe14b9c3
admin admin <evaladmin@example.com>
2022-11-01 17:54:52 +0700

    "User admin wrote content /scripts/classes/org/craftercms/sites/editorial/MultiSitesAwareElasticSearchClient.groovy"

--- scripts/classes/org/craftercms/sites/editorial/MultiSitesAwareElasticSearchClient.groovy
+++ scripts/classes/org/craftercms/sites/editorial/MultiSitesAwareElasticSearchClient.groovy
@@ -5,13 +5,14 @@ import co.elastic.clients.elasticsearch.core.SearchRequest
 import co.elastic.clients.elasticsearch.ElasticsearchClient
 import java.beans.ConstructorProperties
 
-class MultiSitesAwareElasticSearchClient extends AbstractElasticsearchClientWrapper {
+public class MultiSitesAwareElasticSearchClient extends AbstractElasticsearchClientWrapper {
     @ConstructorProperties(["client"])
     public MultiSitesAwareElasticSearchClient(ElasticsearchClient client) {
         super(client)
         println "init method"
         println client
     }
+    
     @Override
     protected void updateIndex(SearchRequest request, Map<String, Object> parameters, RequestUpdates updates) {
         super.updateIndex(request, parameters, updates)   
