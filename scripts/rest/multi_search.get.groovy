3e30403d2a126cc803f25600989a8e4e6ea52fdf
admin admin <evaladmin@example.com>
2022-11-01 16:37:48 +0700

    "User admin wrote content /scripts/rest/multi_search.get.groovy"

--- scripts/rest/multi_search.get.groovy
+++ scripts/rest/multi_search.get.groovy
@@ -28,4 +28,40 @@ println request
 
 println result
 
-return result
+return processUserSearchResults(result)
+
+
+private def processUserSearchResults(result) {
+    def articles = []
+    def hits = result.hits().hits()
+
+    if (hits) {
+      hits.each {hit ->
+        def doc = hit.source()
+        def article = [:]
+            article.id = doc.objectId
+            article.objectId = doc.objectId
+            article.path = doc.localId
+            article.title = doc.title_t
+            article.url = urlTransformationService.transform("storeUrlToRenderUrl", doc.localId)
+
+        if (hit.highlight()) {
+          def articleHighlights = hit.highlight().values()
+          if (articleHighlights) {
+              def highlightValues = []
+
+              articleHighlights.each { value ->
+                  highlightValues.addAll(value)
+              }
+
+              article.highlight = StringUtils.join(highlightValues, "... ")
+              article.highlight = StringUtils.strip(article.highlight)
+          }
+        }
+
+        articles << article
+      }
+    }
+
+    return articles
+  }
