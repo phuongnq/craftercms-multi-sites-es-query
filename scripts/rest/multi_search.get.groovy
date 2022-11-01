f34b03d04f3c6f3981caf21aa8fd447e304db4a5
admin admin <evaladmin@example.com>
2022-11-01 16:34:19 +0700

    "User admin wrote content /scripts/rest/multi_search.get.groovy"

--- scripts/rest/multi_search.get.groovy
+++ scripts/rest/multi_search.get.groovy
@@ -4,7 +4,7 @@ import co.elastic.clients.elasticsearch.core.SearchRequest
 def ARTICLE_CONTENT_TYPE = '/page/article'
 
 def sites = ['editorial-b', 'editorial-c', 'editorial-d']
-def indexes = sites.collect { "${it}_authoring_v1" }
+def indexes = sites.collect { "${it}-authoring_v1" }
 
 println indexes.join(',')
 
