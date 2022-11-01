a4ccd2b2f0e17bf27893e55db619b2b1c1addd7f
admin admin <evaladmin@example.com>
2022-11-01 16:30:55 +0700

    "User admin wrote content /scripts/rest/multi_search.get.groovy"

--- scripts/rest/multi_search.get.groovy
+++ scripts/rest/multi_search.get.groovy
@@ -6,17 +6,19 @@ def ARTICLE_CONTENT_TYPE = '/page/article'
 def sites = ['editorial-b', 'editorial-c', 'editorial-d']
 def indexes = sites.collect { "${it}_authoring_v1" }
 
+println indexes.join(',')
+
 SearchRequest request = SearchRequest.of(r -> r
       .query(q -> q
         .match(m -> m
-            .field("content-type")
+            .field('content-type')
               .query(v -> v
                 .stringValue(ARTICLE_CONTENT_TYPE)
             )
         )
       )
       .from(0)
-      .size(20)
+      .size(10)
       .index(indexes.join(','))
 )
 
