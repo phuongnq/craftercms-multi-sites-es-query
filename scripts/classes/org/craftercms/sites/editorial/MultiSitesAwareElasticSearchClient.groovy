4e8922767586fd9cd31a617c672fcf9fcf1963c2
admin admin <evaladmin@example.com>
2022-11-01 21:17:46 +0700

    "User admin wrote content /scripts/classes/org/craftercms/sites/editorial/MultiSitesAwareElasticSearchClient.groovy"

--- scripts/classes/org/craftercms/sites/editorial/MultiSitesAwareElasticSearchClient.groovy
+++ scripts/classes/org/craftercms/sites/editorial/MultiSitesAwareElasticSearchClient.groovy
@@ -6,6 +6,13 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient
 import java.beans.ConstructorProperties
 
 class MultiSitesAwareElasticSearchClient extends AbstractElasticsearchClientWrapper {
+    @ConstructorProperties(["client"])
+    public MultiSitesAwareElasticSearchClient(ElasticsearchClient client) {
+        super(client)
+        println "init method"
+        println client
+    }
+    
     def search() {
         return "aaa"
     }
